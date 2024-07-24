4e1bdacf4da972c75ed05538a15f9d5e4005798b
Mildakhusnul26 <mildakhusnul999@gmail.com>
2024-07-24 19:42:08 +0700

    debug

--- database/seeders/RekapSeeder.php
+++ database/seeders/RekapSeeder.php
@@ -35,7 +35,7 @@ class RekapSeeder extends Seeder
         Rekap::create([
             'nama' => 'Agfinita Gusti Hikmawani',
             'nim' => '2131740006',
-            'semester' => '4',
+            'semester' => '6',
             'kompen' => '6',
             'email' => '2131740006@gmail.com',
         ]);
@@ -43,7 +43,7 @@ class RekapSeeder extends Seeder
         Rekap::create([
             'nama' => 'Rizky Putri Meilia',
             'nim' => '2131740024',
-            'semester' => '4',
+            'semester' => '6',
             'kompen' => '1',
             'email' => '2131740024@gmail.com',
         ]);
@@ -51,7 +51,7 @@ class RekapSeeder extends Seeder
         Rekap::create([
             'nama' => 'Nurul Hikmah',
             'nim' => '2131740020',
-            'semester' => '2',
+            'semester' => '6',
             'kompen' => '8',
             'email' => '2131740020@gmail.com',
         ]);
@@ -59,17 +59,57 @@ class RekapSeeder extends Seeder
         Rekap::create([
             'nama' => 'Neisha Rindiani Armilia',
             'nim' => '2131740035',
-            'semester' => '2',
-            'kompen' => '5',
+            'semester' => '6',
+            'kompen' => '6',
             'email' => '2131740035@gmail.com',
         ]);
 
         Rekap::create([
             'nama' => 'Fira Ila Nufika',
             'nim' => '2131740015',
-            'semester' => '5',
+            'semester' => '6',
             'kompen' => '3',
             'email' => '2131740015@gmail.com',
+        ]);
+
+        Rekap::create([
+            'nama' => 'Aisa Dwi Yana',
+            'nim' => '2331740011',
+            'semester' => '5',
+            'kompen' => '6',
+            'email' => '2331740011@student.polinema.ac.id',
+        ]);
+
+        Rekap::create([
+            'nama' => 'Mochamad Calvin Rozil',
+            'nim' => '2231740047',
+            'semester' => '5',
+            'kompen' => '4',
+            'email' => '2231740047@student.polinema.ac.id',
+        ]);
+
+        Rekap::create([
+            'nama' => 'Arief Budi Raharjo',
+            'nim' => '2131740043',
+            'semester' => '5',
+            'kompen' => '8',
+            'email' => '2131740043@gmail.com',
+        ]);
+
+        Rekap::create([
+            'nama' => 'Fannisa Azzahra',
+            'nim' => '2231740037',
+            'semester' => '5',
+            'kompen' => '6',
+            'email' => '2231740037@student.polinema.ac.id',
+        ]);
+
+        Rekap::create([
+            'nama' => 'Dava Anugrah Illahi Putra',
+            'nim' => '2231740036',
+            'semester' => '5',
+            'kompen' => '8',
+            'email' => '2231740036@student.polinema.ac.id',
         ]);
 
     }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,16 +20,24 @@ class UserSeeder extends Seeder
         ]);
 
         User::create([
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
+            'name' => 'Dion Cahya',
+            'email' => 'dioncahya@polinema.ac.id',
             'email_verified_at' => now(),
             'role' => 'superadmin',
             'password' => Hash::make('password'),
         ]);
 
         User::create([
-            'name' => 'Dosen',
-            'email' => 'dosen@gmail.com',
+            'name' => 'Alvionitha Sara',
+            'email' => 'alvionithasara@polinema.ac.id',
+            'email_verified_at' => now(),
+            'role' => 'admin',
+            'password' => Hash::make('password'),
+        ]);
+
+        User::create([
+            'name' => 'Very Sugiarto',
+            'email' => 'very.sugiarto@polinema.ac.id',
             'email_verified_at' => now(),
             'role' => 'admin',
             'password' => Hash::make('password'),
@@ -37,7 +45,7 @@ class UserSeeder extends Seeder
 
         User::create([
             'name' => 'Milda Khusnul Khotimah',
-            'email' => '2131740026@gmail.com',
+            'email' => '2131740026@student.polinema.ac.id',
             'email_verified_at' => now(),
             'role' => 'user',
             'password' => Hash::make('password'),
@@ -45,7 +53,7 @@ class UserSeeder extends Seeder
 
         User::create([
             'name' => 'Nadhirotul Laily',
-            'email' => '2131740041@gmail.com',
+            'email' => '2131740041@student.polinema.ac.id',
             'email_verified_at' => now(),
             'role' => 'user',
             'password' => Hash::make('password'),
@@ -53,7 +61,7 @@ class UserSeeder extends Seeder
 
         User::create([
             'name' => 'Agfinita Gusti Hikmawani',
-            'email' => '2131740006@gmail.com',
+            'email' => '2131740006@student.polinema.ac.id',
             'email_verified_at' => now(),
             'role' => 'user',
             'password' => Hash::make('password'),
@@ -61,7 +69,7 @@ class UserSeeder extends Seeder
 
         User::create([
             'name' => 'Rizky Putri Meilia',
-            'email' => '2131740024@gmail.com',
+            'email' => '2131740024@student.polinema.ac.id',
             'email_verified_at' => now(),
             'role' => 'user',
             'password' => Hash::make('password'),
@@ -69,7 +77,7 @@ class UserSeeder extends Seeder
 
         User::create([
             'name' => 'Nurul Hikmah',
-            'email' => '2131740020@gmail.com',
+            'email' => '2131740020@student.polinema.ac.id',
             'email_verified_at' => now(),
             'role' => 'user',
             'password' => Hash::make('password'),
@@ -77,7 +85,7 @@ class UserSeeder extends Seeder
 
         User::create([
             'name' => 'Neisha Rindiani Armilia',
-            'email' => '2131740035@gmail.com',
+            'email' => '2131740035@student.polinema.ac.id',
             'email_verified_at' => now(),
             'role' => 'user',
             'password' => Hash::make('password'),
@@ -85,7 +93,47 @@ class UserSeeder extends Seeder
 
         User::create([
             'name' => 'Fira Ila Nufika',
-            'email' => '2131740015@gmail.com',
+            'email' => '2131740015@student.polinema.ac.id',
+            'email_verified_at' => now(),
+            'role' => 'user',
+            'password' => Hash::make('password'),
+        ]);
+
+        User::create([
+            'name' => 'Aisa Dwi Yana',
+            'email' => '2331740011@student.polinema.ac.id',
+            'email_verified_at' => now(),
+            'role' => 'user',
+            'password' => Hash::make('password'),
+        ]);
+
+        User::create([
+            'name' => 'Mochamad Calvin Rozil',
+            'email' => '2231740047@student.polinema.ac.id',
+            'email_verified_at' => now(),
+            'role' => 'user',
+            'password' => Hash::make('password'),
+        ]);
+
+        User::create([
+            'name' => 'Arief Budi Raharjo',
+            'email' => '2131740043@student.polinema.ac.id',
+            'email_verified_at' => now(),
+            'role' => 'user',
+            'password' => Hash::make('password'),
+        ]);
+
+        User::create([
+            'name' => 'Fannisa Azzahra',
+            'email' => '2231740037@student.polinema.ac.id',
+            'email_verified_at' => now(),
+            'role' => 'user',
+            'password' => Hash::make('password'),
+        ]);
+
+        User::create([
+            'name' => 'Dava Anugrah Illahi Putra',
+            'email' => '2231740036@student.polinema.ac.id',
             'email_verified_at' => now(),
             'role' => 'user',
             'password' => Hash::make('password'),
